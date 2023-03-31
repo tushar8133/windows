5c5c691fa52ea91ce1d69ca561432122f0450386
Tushar Kumar <tushar8133@gmail.com>
2023-03-31 13:35:03 +0530

    Update delete folder.bat

--- delete folder.bat
+++ delete folder.bat
@@ -1,8 +1,8 @@
-@ECHO OFF
-ECHO Delete Folder: %CD%?
-PAUSE
-SET FOLDER=%CD%
-CD /
-DEL /F/Q/S "%FOLDER%" > NUL
-RMDIR /Q/S "%FOLDER%"
+@ECHO off
+Mode con cols=60 lines=20
+TITLE DELETE FOLDER
+Color 4F
+SET /P mypath="DRAG THE FOLDER HERE ==> "
+DEL /F /Q /S "%mypath%" > NUL
+RMDIR /Q /S "%mypath%"
 EXIT
